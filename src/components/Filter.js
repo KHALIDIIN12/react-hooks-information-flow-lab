ab90429b27072da2830306509407d6482016ddb5
KHALIDIIN12 <khalidinho53@gmail.com>
2025-07-27 16:49:28 +0300

    All components and tests passing

---
+++ src/components/Filter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Filter({ onCategoryChange, selectedCategory }) {
+  return (
+    <div className="Filter">
+      <select name="filter" onChange={onCategoryChange} value={selectedCategory}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+        <option value="Meat">Meat</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
